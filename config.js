f824654973b0e5356338cb07d56654290192067d
pathumamantha <139663361+pathumamantha@users.noreply.github.com>
2023-07-16 07:27:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94726013156', 'GURU', true],
+  ['94726013156', 'SAVIYA', true], 
   [''] 
 ] //Number of owners
 
